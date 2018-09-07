9ac6444ff7bc0805e7ad6904c904d59faa58bc13
tuckersjomeling <tuckersjomeling@users.noreply.github.com>
2018-09-07 17:57:06 -0500

    changed line 38
    changed numberOfDigits to the variable that was declared earlier in the code, numDigits

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -35,7 +35,7 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
     modulous *= 10;
 
